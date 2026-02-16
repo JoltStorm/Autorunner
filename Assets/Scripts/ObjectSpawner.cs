e1acf7126428056a483d5e958dee414ccffef99b
JoltStorm <orcam20098@gmail.com>
2026-02-15 18:41:22 -0600

    Adjusted player, added base for ObjectSpawner
    changed player sprite, size, and collision box.

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,24 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+
+    [SerializeField] private int tokenCount;
+    [SerializeField] private float timePassed;
+
+    [Header("Hazards")]
+    public GameObject tripleSpike;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
